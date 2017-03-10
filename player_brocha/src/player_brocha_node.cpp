ef2cc5b2cc3a49248b58c44e71ae99752c9ab060
bruno <bruno.rocha@ua.pt>
2017-03-10 13:59:57 +0000

    alteracao

--- player_brocha/src/player_brocha_node.cpp
+++ player_brocha/src/player_brocha_node.cpp
@@ -1,88 +1,127 @@
 #include <iostream>  
-#include <vector>                                                                                   
-#include <rwsua2017_libs.h>
+#include <vector>    
+//ROS INCLUDES
+#include <ros/ros.h>  
+
+#include <rwsua2017_libs/player.h>   
+#include <rwsua2017_msgs/MakeAPlay.h>                                                                            
 
 using namespace std;
-namespace rwsua2017
-{
 
-	class Player
-	{
-		public:
+namespace rwsua2017 {
 
-		Player(string argin_name, string argin_team_name="blue")
-		{
-			cout << "player name " << argin_name << endl;
-			this->name = argin_name;
-			this->team_name = argin_team_name;
-			set_team_name(argin_team_name);
-		}
+        /*
+            class Player
+            {
+                    public:
 
-		string name;
-		
-		//accessor SET
-		void set_team_name(string argin_team_name)
-		{
-			if (argin_team_name == "red" || argin_team_name == "green" || argin_team_name == "blue")
-				this ->team_name=argin_team_name;
+                    Player(string argin_name, string argin_team_name="blue")
+                    {
+                            cout << "player name " << argin_name << endl;
+                            this->name = argin_name;
+                            this->team_name = argin_team_name;
+                            set_team_name(argin_team_name);
+                    }
 
-			else
-				cout<< "Error: incorrect team name " << endl;
-		}
+                    string name;
 		
-		//Overloaded accessor
-		void set_team_name(void)
-		{
-			set_team_name("red"); // default value
-		}
+                    //accessor SET
+                    void set_team_name(string argin_team_name)
+                    {
+                            if (argin_team_name == "red" || argin_team_name == "green" || argin_team_name == "blue")
+                                    this ->team_name=argin_team_name;
+
+                            else
+                                    cout<< "Error: incorrect team name " << endl;
+                    }
 		
-		//accessor SET
-		string get_team_name(void)
-		{
-			return this->team_name;
-		}
+                    //Overloaded accessor
+                    void set_team_name(void)
+                    {
+                            set_team_name("red"); // default value
+                    }
+		
+                    //accessor SET
+                    string get_team_name(void)
+                    {
+                            return this->team_name;
+                    }
 		
 		
-		private:
-		string team_name;
-	};
+                    private:
+                    string team_name;
+            };
 	
-	
-	class MyPlayer: public Player //inherits the class Player
-	{
-		public:
-		
-		MyPlayer(string argin_name, string argin_team_name): Player (argin_name, argin_team_name)
-		{
-			cout<< "Initialized MyPlayer"<<endl;
-		}
-		
-		vector<string> teammates;
-	};
+     */
+
+    class MyPlayer : public Player //inherits the class Player
+    {
+    public:
+
+        ros::NodeHandle n;
+        ros::Subscriber sub;
+        vector<string> red_team;
+        vector<string> green_team;
+        vector<string> blue_team;
+
+        MyPlayer(string argin_name, string argin_team_name) : Player(argin_name, argin_team_name) {
+
+            n.getParam("red", red_team);
+            n.getParam("green", green_team);
+            n.getParam("blue", blue_team);
+
+
+            cout << "red_team:"<<endl;
+            for (size_t i=0;i<red_team.size();i++)
+                cout << red_team[i]<<endl;
+            
+            cout << "green_team:"<<endl;
+            for (size_t i=0;i<green_team.size();i++)
+                cout << green_team[i]<<endl;
+            
+            cout << "blue_team:"<<endl;
+            for (size_t i=0;i<blue_team.size();i++)
+                cout << blue_team[i]<<endl;
+
+            sub = n.subscribe("/make_a_play", 1000, &MyPlayer::makeAPlayCallback, this);
+            cout << "Initialized MyPlayer" << endl;
+        }
+
+        void makeAPlayCallback(const rwsua2017_msgs::MakeAPlay::ConstPtr& msg) {
+            cout << "msg: max displacement -> " << msg->max_displacement << endl;
+        }
+
+
+        vector<string> teammates;
+    };
 }
 
-int main()
-{
-	//because we used <using namespace std>  we can replace the other line
-	//std::cout << "Hello world" << std::endl;
-	cout << "Hello world" << endl;
-  
+int main(int argc, char **argv) {
+    //because we used <using namespace std>  we can replace the other line
+    //std::cout << "Hello world" << std::endl;
+    cout << "Hello world" << endl;
+
+    ros::init(argc, argv, "player_brocha");
+
     //Creating an instance of class Player
-    rws2017::MyPlayer myplayer("brocha","red");
+    rwsua2017::MyPlayer myplayer("brocha", "red");
 
     //cout << "Created an instance of class player with public name " << player.name << endl;
     cout << "name = " << myplayer.name << endl;
-    cout << "team name = " << myplayer.get_team_name()<<endl;
-    
+    cout << "team name = " << myplayer.get_team_name() << endl;
+
     myplayer.teammates.push_back("rodolfo");
     myplayer.teammates.push_back("arnaldo");
-       
+
+
+
     // size_t = unsigned long int (sortchut)
-    for(size_t i=0; i<myplayer.teammates.size(); ++i)
-    {
-		cout<<myplayer.teammates[i]<<endl;
-	}
-    
+    for (size_t i = 0; i < myplayer.teammates.size(); ++i) {
+        cout << myplayer.teammates[i] << endl;
+    }
+
+    ros::spin();
+
     return 1;
 }
 
