e5a7fb43cb767ec2c7351d592a22011af255a875
bruno <bruno.rocha@ua.pt>
2017-03-17 09:34:33 +0000

    alteracao

--- player_brocha/src/player_brocha_node.cpp
+++ player_brocha/src/player_brocha_node.cpp
@@ -14,101 +14,89 @@ using namespace ros;
 
 namespace rwsua2017 {
 
-       
     class MyPlayer : public Player //inherits the class Player
     {
     public:
-	
+
         //properties
         Subscriber sub;
-        TransformBroadcaster br; 
-		Transform t1;
-		TransformListener listener;
-		
+        TransformBroadcaster br;
+        Transform t1;
+        TransformListener listener;
+
         MyPlayer(string argin_name, string argin_team_name) : Player(argin_name, argin_team_name) {
 
-           		
+
             sub = n.subscribe("/make_a_play/cheetah", 1000, &MyPlayer::makeAPlayCallback, this);
             cout << "Initialized MyPlayer" << endl;
-			
-			t1.setOrigin( Vector3(randNumber(),randNumber(), 0.0));
+
+            t1.setOrigin(Vector3(randNumber(), randNumber(), 0.0));
             Quaternion q;
-			q.setRPY(0, 0,0);
-			t1.setRotation(q);
-			br.sendTransform(StampedTransform(t1, Time::now(), "map", name));
-			
-        
-        
+            q.setRPY(0, 0, 0);
+            t1.setRotation(q);
+            br.sendTransform(StampedTransform(t1, Time::now(), "map", name));
+        };
+
+        double randNumber() {
+            struct timeval t1;
+            gettimeofday(&t1, NULL);
+            srand(t1.tv_usec);
+            double x = ((((double) rand() / (double) RAND_MAX)*2 - 1)*5);
+
+            return x;
         }
-        
-			double randNumber(){
-				struct timeval t1;
-				gettimeofday(&t1,NULL);
-				srand(t1.tv_usec);
-				double x =((((double)rand()/(double)RAND_MAX)*2 -1)*5);
 
-				return x;
-			}
+        float getAngleTo(string player_name) {
 
-		
-		double getAngleTo(string name){
-			
-				
-			   StampedTransform trans;
-			   
-			try{
-				listener.lookupTransform("/brocha", name,Time(0), trans);
-				}
-			catch (TransformException &ex) {
-				
-				ROS_ERROR("%s",ex.what());
-				Duration(1.0).sleep();
-			}
-						
-			float x=trans.getOrigin().y();
-			float y=trans.getOrigin().x();
-			
-			float angle=atan2(y,x);
-			return angle;
-			
-			
-			
-		}
-		
-		
+            StampedTransform trans;
+
+            try {
+                listener.lookupTransform(name, player_name, Time(0), trans);
+
+            } catch (TransformException &ex) {
+
+                ROS_ERROR("%s", ex.what());
+                Duration(0.01).sleep();
+            }4
+
+            float x = trans.getOrigin().x();
+            float y = trans.getOrigin().y();
+
+            return atan2(y, x);
+        }
+
         void makeAPlayCallback(const rwsua2017_msgs::MakeAPlay::ConstPtr& msg) {
-			
-								
+
+
             cout << "msg: max displacement -> " << msg->max_displacement << endl;
             //send a information basic to move player
-            float turn_angle=getAngleTo("bvieira");
-            float displacement = 0.5;
-            double max_t =  (M_PI/30);
-			
-			
-			if (turn_angle > max_t) turn_angle = max_t;
-			else if (turn_angle < -max_t) turn_angle = -max_t;
-			
+            float turn_angle = getAngleTo("bvieira");
+            float displacement = msg->max_displacement;
+
+            double max_t = (M_PI / 30);
+
+            if (turn_angle > max_t) turn_angle = max_t;
+            else if (turn_angle < -max_t) turn_angle = -max_t;
+
             //compute the new reference frame
             Transform t_mov;
             Quaternion q;
             q.setRPY(0, 0, turn_angle);
-            
+
             t_mov.setRotation(q);
-            t_mov.setOrigin( Vector3(displacement,0.0, 0.0) );
-            
-			Transform t=t1 * t_mov;
-			
-			//Send the new transform to ROS
-					
-			br.sendTransform(StampedTransform(t, Time::now(), "/map", name));
-			
-			t1=t;
-			
-			//caracteristics to  move the player
-			
-			         
+            t_mov.setOrigin(Vector3(displacement, 0.0, 0.0));
+
+            Transform t = t1 * t_mov;
+
+            //Send the new transform to ROS
+
+            br.sendTransform(StampedTransform(t, Time::now(), "/map", name));
+            t1 = t;
+
         }
+
+
+
 
         vector<string> teammates;
     };
@@ -126,15 +114,10 @@ int main(int argc, char **argv) {
 
     //cout << "Created an instance of class player with public name " << player.name << endl;
     cout << "name = " << myplayer.name << endl;
-    cout << "team name = " << myplayer.get_team_name() << endl;
-
-    myplayer.teammates.push_back("rodolfo");
-    myplayer.teammates.push_back("arnaldo");
-
-
 
     // size_t = unsigned long int (sortchut)
     for (size_t i = 0; i < myplayer.teammates.size(); ++i) {
+
         cout << myplayer.teammates[i] << endl;
     }
 
