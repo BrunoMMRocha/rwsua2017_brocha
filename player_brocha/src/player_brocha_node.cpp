993d548b5fcb131d4a90c1874a93f50ab06c324c
bruno <bruno.rocha@ua.pt>
2017-03-17 12:45:38 +0000

    com bocas

--- player_brocha/src/player_brocha_node.cpp
+++ player_brocha/src/player_brocha_node.cpp
@@ -6,6 +6,7 @@
 #include <rwsua2017_msgs/MakeAPlay.h>                                                                            
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
+#include <visualization_msgs/Marker.h>
 
 using namespace std;
 //using namespace boost;
@@ -23,15 +24,17 @@ namespace rwsua2017 {
         TransformBroadcaster br;
         //Transform t1; //memoria local do jogo (cada um dos jogos individuais)//é necessário actualizar sempre que for chamada
         TransformListener listener;
+        ros::Publisher vis_pub;
         
         MyPlayer(string argin_name, string argin_team_name) : Player(argin_name, argin_team_name) {
-            
-            
+
+
             sub = n.subscribe("/make_a_play/cheetah", 1000, &MyPlayer::makeAPlayCallback, this);
             cout << "Initialized MyPlayer" << endl;
-            
+             vis_pub = n.advertise<visualization_msgs::Marker>( "/bocas", 0 );
+             
             Transform t1;
-            
+
             t1.setOrigin(Vector3(randNumber(), randNumber(), 0.0));
             Quaternion q;
             q.setRPY(0, 0, 0);
@@ -68,38 +71,38 @@ namespace rwsua2017 {
 
             return atan2(y, x);
         }
-        
-        tf::StampedTransform getPose( float time_to_wait = 0.1){
+
+        tf::StampedTransform getPose(float time_to_wait = 0.1) {
             //função vai atualizar o sistema e vai meter todos em sincronizmo
             //Transform t1;
             StampedTransform trans;
-            Time now = Time(0);    
+            Time now = Time(0);
             try {
                 listener.waitForTransform("map", name, now, Duration(time_to_wait));
-                listener.lookupTransform("map",name , now, trans);
+                listener.lookupTransform("map", name, now, trans);
 
             } catch (TransformException &ex) {
 
                 ROS_ERROR("%s", ex.what());
                 Duration(0.01).sleep();
             }
-                                               
+
             return trans;
-            
+
         }
-        
-        void move(float displacement,float max_displacement, float turn_angle,float max_turn_angle){
-      
+
+        void move(float displacement, float max_displacement, float turn_angle, float max_turn_angle) {
+
             //float displacement = msg->max_displacement;
-                        
+
             //max turn angle
             if (turn_angle > max_turn_angle) turn_angle = max_turn_angle;
             else if (turn_angle < -max_turn_angle) turn_angle = -max_turn_angle;
             //max displacmente
             if (displacement > max_displacement) displacement = max_displacement;
             else if (displacement < -max_displacement) displacement = -max_displacement;
-            
-            
+
+
             //compute the new reference frame
             Transform t_mov;
             Quaternion q;
@@ -107,90 +110,116 @@ namespace rwsua2017 {
 
             t_mov.setRotation(q);
             t_mov.setOrigin(Vector3(displacement, 0.0, 0.0));
-            
+
             Transform t = getPose() * t_mov;
 
             //Send the new transform to ROS
 
             br.sendTransform(StampedTransform(t, Time::now(), "/map", name));
-            
-       
+
+
         }
-        
-        string getNear(){
+
+        string getNear() {
             float time_to_wait = 0.1;
-            
+
             StampedTransform trans1;
             StampedTransform trans2;
             StampedTransform trans3;
-            
+
             ros::Time now = Time(0);
-            
+
             try {
-                
+
                 listener.waitForTransform(name, "fsilva", now, Duration(time_to_wait));
                 listener.lookupTransform(name, "fsilva", now, trans1);
-                
+
                 listener.waitForTransform(name, "jferreira", now, Duration(time_to_wait));
                 listener.lookupTransform(name, "jferreira", now, trans2);
-                
+
                 listener.waitForTransform(name, "rmartins", now, Duration(time_to_wait));
                 listener.lookupTransform(name, "rmartins", now, trans3);
-                
-                
+
+
             } catch (TransformException &ex) {
 
                 ROS_ERROR("%s", ex.what());
                 Duration(0.01).sleep();
             }
-                       
-            float d1,d2,d3;
+
+            float d1, d2, d3;
             string playerF;
-            
+
             float x1 = trans1.getOrigin().x();
-            float y1 = trans1.getOrigin().y();         
-            
+            float y1 = trans1.getOrigin().y();
+
             float x2 = trans2.getOrigin().x();
             float y2 = trans2.getOrigin().y();
-            
+
             float x3 = trans3.getOrigin().x();
             float y3 = trans3.getOrigin().y();
-            
-            d1=sqrt(x1 * x1 + y1 * y1);
-            d2=sqrt(x2 * x2 + y2 * y2);
-            d3=sqrt(x3 * x3 + y3 * y3);
-            
-            if(d1<d2 && d1<d3){ playerF="fsilva";
-            
-            }else if(d2<d3 && d2<d1){
-                   playerF="jferreira";}
-                   
-            else{playerF="rmartins";
+
+            d1 = sqrt(x1 * x1 + y1 * y1);
+            d2 = sqrt(x2 * x2 + y2 * y2);
+            d3 = sqrt(x3 * x3 + y3 * y3);
+
+            if (d1 < d2 && d1 < d3) {
+                playerF = "fsilva";
+            } else if (d2 < d3 && d2 < d1) {
+                playerF = "jferreira";
             }
-                      
-            
-            return playerF ;
-            
-            
+            else {
+                playerF = "rmartins";
+            }
+
+
+
+            return playerF;
+
+
         }
-        
-        
+
         void makeAPlayCallback(const rwsua2017_msgs::MakeAPlay::ConstPtr& msg) {
-            
-                  //send a information basic to move player
+
+            //send a information basic to move player
             //getNearbyPlayer();
             //etNear
             //float turn_angle = getAngleTo("fsilva");
             //cout << getNear();
-            
+
             float turn_angle = getAngleTo(getNear());
-            
-            float displacement =msg->max_displacement;
+
+            float displacement = msg->max_displacement;
             //float max_turn_angle = (M_PI / 30);
-                        
+
             cout << "msg: max displacement -> " << msg->max_displacement << endl;
-            
-            move(displacement, msg->max_displacement, turn_angle, M_PI/30);      
+
+            move(displacement, msg->max_displacement, turn_angle, M_PI / 30);
+
+            //enviar boca
+            visualization_msgs::Marker marker;
+            marker.header.frame_id = name;
+            marker.header.stamp = ros::Time();
+            marker.ns = name;
+            marker.id = 0;
+            marker.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
+            marker.action = visualization_msgs::Marker::ADD;
+            marker.pose.position.x = 0;
+            marker.pose.position.y = 0.4;
+            marker.pose.position.z = 0;
+            marker.pose.orientation.x = 0.0;
+            marker.pose.orientation.y = 0.0;
+            marker.pose.orientation.z = 0.0;
+            marker.pose.orientation.w = 1.0;
+            marker.scale.z = 0.4;
+            marker.color.a = 1.0; // Don't forget to set the alpha!
+            marker.color.r = 0.3;
+            marker.color.g = 0.3;
+            marker.color.b = 0.3;
+            marker.frame_locked = 1;
+            marker.lifetime = ros::Duration(1);
+            marker.text = " :D ";
+            vis_pub.publish(marker);
 
         }
         vector<string> teammates;
